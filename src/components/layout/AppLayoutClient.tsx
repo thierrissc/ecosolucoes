b62ce7f35b944f09ec1ac9a7a627d1b13ca7c8a5
Thierri do Carmo <thshadow123@gmail.com>
2026-09-17 14:47:00 -0300

    remocao de transicao de fundo no carregamento da tela

--- src/components/layout/AppLayoutClient.tsx
+++ src/components/layout/AppLayoutClient.tsx
@@ -7,7 +7,7 @@ export default function AppLayoutClient({ children }: { children: React.ReactNod
   const pathname = usePathname();
 
   return (
-    <div className="min-h-screen bg-surface-0 transition-colors duration-400 w-full flex flex-col">
+    <div className="min-h-screen bg-surface-0 w-full flex flex-col">
       <TopNav pathname={pathname} />
       <main className="flex-1 w-full px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-6 md:py-8">
         <div className="w-full">
